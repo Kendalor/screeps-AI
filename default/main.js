6de48537bdaab8e57567e401fa9df4a8bc910efd
Patrick <prehn@students.uni-mainz.de>
2016-12-04 23:24:14 +0100

    biggest add

---
+++ default/main.js
@@ -0,0 +1,141 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+MAX_HARVESTERS=15
+MAX_BUILDERS=5
+MAX_UPGRADERS=5
+MIN_BUILDERS=1
+MIN_UPGRADERS=1
+MIN_HARVESTERS=5
+ACC_CPU=0
+var WORKER_PRESETS = [[WORK,CARRY,MOVE],[WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE]];
+
+//Lets first add a shortcut prototype to the sources memory:
+Source.prototype.memory = undefined;
+
+
+
+
+
+module.exports.loop = function () {
+
+    if(Game.time % 5000 == 0){
+        Game.spawns['Spawn1'].room.controller.activateSafeMode();
+    }
+    for(var roomName in Game.rooms){//Loop through all rooms your creeps/structures are in
+        var room = Game.rooms[roomName];
+        if(!room.memory.sources){//If this room has no sources memory yet
+            room.memory.sources = {}; //Add it
+            var sources = room.find(FIND_SOURCES);//Find all sources in the current room
+            for(var i in sources){
+                var source = sources[i];
+                source.memory = room.memory.sources[source.id] = {}; //Create a new empty memory object for this source
+                //Now you can do anything you want to do with this source
+                //for example you could add a worker counter:
+                source.memory.workers = 0;
+                source.memory.efficiency =0;
+            }
+        }
+
+    }
+
+
+
+
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+
+    var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+
+    //Get Tick for reset
+    if(Game.time % 10 == 0){
+        console.log('calculating source efficiency');
+        for(var name in Game.rooms){
+            var sources = Game.rooms[name].find(FIND_SOURCES);
+            for(i in sources){
+                var source = sources[i];
+                Game.rooms[name].memory.sources[source.id].nextReset= source.ticksToRegeneration;
+            }
+
+
+
+        }
+    }/**/
+
+
+
+
+
+
+    if(Game.time % 5 == 0){
+    for(var name in Game.rooms) {
+        var structures = Game.rooms[name].find(FIND_STRUCTURES,{filter: (structure) => structure.structureType == STRUCTURE_TOWER});
+        for( i in structures){
+            if(structures[i]) {
+                var closestDamagedStructure = structures[i].pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: (structure) => structure.hits < structure.hitsMax
+                });
+                if(closestDamagedStructure) {
+                    structures[i].repair(closestDamagedStructure);
+                }
+
+                var closestHostile = structures[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                if(closestHostile) {
+                    structures[i].attack(closestHostile);
+                }
+            }
+        }
+        if(Game.time % 20 == 0){
+            console.log('Aktuallisiere GSpieltaktik');
+            var energyCap=Game.rooms[name].energyCapacityAvailable;
+            var controlerlvl = Game.rooms[name].controller.level;
+            if(controlerlvl == 1){
+                Game.rooms[name].memory.defaultworker = 0;
+            }else if(controlerlvl == 2 && energyCap == 550){
+                Game.rooms[name].memory.defaultworker = 1;
+            }
+        }
+
+        //Berechne Effizienz
+        /*
+        if(Game.time == Game.rooms[name].memory,
+        */
+
+        var energyRatio = Game.rooms[name].energyAvailable/Game.rooms[name].energyCapacityAvailable;
+
+
+        if(harvesters.length < MAX_HARVESTERS && Game.rooms[name].energyAvailable  && Game.spawns['Spawn1'].canCreateCreep(WORKER_PRESETS[Game.rooms[name].memory.defaultworker], undefined, {role: 'harvester'}) == 0){
+            var newName = Game.spawns['Spawn1'].createCreep(WORKER_PRESETS[Game.rooms[name].memory.defaultworker], undefined, {role: 'harvester'});
+            console.log('Spawning new harvester: ' + newName);
+        }
+
+
+        console.log('Room "'+name+'" has  Energy: '+Game.rooms[name].energyAvailable+' Harvesters: '+harvesters.length+' Current Workerpreset '+ WORKER_PRESETS[Game.rooms[name].memory.defaultworker]);
+    }
+    }
+
+
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+    //var temp=Game.cpu.getUsed();
+    //console.log('CPU Used: '+ temp);
+}
---
+++ default/role.harvester.js
@@ -0,0 +1,85 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if((creep.carry.energy == 0 && !creep.memory.harvesting )|| !creep.memory.targetId && creep.memory.harvesting) {
+            creep.say('harvesting');
+            creep.memory.harvesting=true;
+            var sources = creep.room.find(FIND_SOURCES);
+            //find nearest source
+
+            var targetId=sources[0].id;
+            for(var i in sources){
+                console.log('TAGET SETTING')
+                console.log(creep.room.memory.sources[targetId].workers)
+                console.log(creep.room.memory.sources[sources[i].id].workers)
+                console.log(creep.room.memory.sources[targetId].workers < creep.room.memory.sources[sources[i].id].workers)
+                if(creep.room.memory.sources[targetId].workers > creep.room.memory.sources[sources[i].id].workers){
+                    targetId=sources[i].id;
+                }else{
+
+                }
+            }
+            creep.memory.targetId=targetId;
+            creep.room.memory.sources[targetId].workers = 0;
+	    }else if(creep.carry.energy == creep.carryCapacity && creep.memory.harvesting ) {
+	        creep.memory.harvesting = false;
+	        creep.room.memory.sources[creep.memory.targetId].workers = 0;
+	        delete creep.memory.targetId;
+	        creep.say('spending');
+	    }else{
+            /*if(creep.memory.harvesting) {
+                var sources = creep.room.find(FIND_SOURCES);
+                if(sources[1].energy > 0){
+                    var target= sources[1];
+                    }else{
+                    var target= sources[0];
+                    }
+                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                    creep.say('mv Source')
+                    }
+                }*/
+
+            if(creep.memory.harvesting) {
+                var target= Game.getObjectById(creep.memory.targetId);
+                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+
+                }
+            }else{
+                var targets_energy = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+                });
+                if(targets_energy.length > 0) {
+                    if(creep.transfer(targets_energy[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets_energy[0]);
+
+                    }
+                }else{
+                    var targets_constr = creep.room.find(FIND_CONSTRUCTION_SITES);
+                    if(targets_constr.length) {
+                        if(creep.build(targets_constr[0]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets_constr[0]);
+
+                        }
+                    }else{
+                        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(creep.room.controller);
+
+                    }
+
+                }
+
+            }
+        }
+	    }
+	}
+};
+
+module.exports = roleHarvester;
