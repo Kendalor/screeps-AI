71054b83c09e8e3a18ea2e166131dda15636ea72
Patrick <prehn@students.uni-mainz.de>
2016-12-06 20:58:39 +0100

    asd

---
+++ backup/role.harvester.js
@@ -0,0 +1,86 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if((creep.carry.energy == 0 && !creep.memory.harvesting )|| !creep.memory.targetId && creep.memory.harvesting) {
+            creep.say('harvesting');
+            creep.memory.harvesting=true;
+            var sources = creep.room.find(FIND_SOURCES);
+            //find nearest source
+
+            var targetId=sources[0].id;
+            for(var i in sources){
+                //console.log('TAGET SETTING')
+                //console.log(creep.room.memory.sources[targetId].workers)
+                //console.log(creep.room.memory.sources[sources[i].id].workers)
+                //console.log(creep.room.memory.sources[targetId].workers < creep.room.memory.sources[sources[i].id].workers)
+                if(creep.room.memory.sources[targetId].workers > creep.room.memory.sources[sources[i].id].workers){
+                    targetId=sources[i].id;
+                }else{
+
+                }
+            }
+            creep.memory.targetId=targetId;
+            creep.room.memory.sources[targetId].workers = creep.room.memory.sources[targetId].workers+1;
+	    }else if(creep.carry.energy == creep.carryCapacity && creep.memory.harvesting ) {
+	        creep.memory.harvesting = false;
+	        creep.room.memory.sources[creep.memory.targetId].workers = creep.room.memory.sources[creep.memory.targetId].workers-1;
+	        delete creep.memory.targetId;
+	        creep.say('spending');
+	    }else{
+            if(creep.memory.harvesting){
+                var target= Game.getObjectById(creep.memory.targetId);
+                if(creep.harvest(target) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+
+                }
+            }else{
+                var targets_energy = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN /*||
+                                structure.structureType == STRUCTURE_TOWER*/) && structure.energy < structure.energyCapacity;
+                    }
+                });
+                if(targets_energy.length > 0) {
+
+                    var target_energy=creep.pos.findClosestByPath(targets_energy);
+                    if(creep.transfer(target_energy, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target_energy);
+
+                    }
+                }else{
+                    var targets_constr_1 = creep.room.find(FIND_CONSTRUCTION_SITES);
+                    if(targets_constr_1.length) {
+                        var targets_constr = creep.pos.findClosestByPath(targets_constr_1);
+                        if(creep.build(targets_constr) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(targets_constr);
+
+                        }
+                    }else{
+                        var targets_repair = creep.room.find(FIND_STRUCTURES, {
+                            filter: (structure) => structure.hits < structure.hitsMax-2500 && structure.structureType != STRUCTURE_WALL
+                        });
+
+                        if(targets_repair.length){
+                            var targets_repair_1 = creep.pos.findClosestByPath(targets_repair);
+                            if(creep.repair(targets_repair_1) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(targets_repair_1);
+                            }
+                        }else{
+                            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                                creep.moveTo(creep.room.controller);
+
+                            }
+                        }
+
+                }
+
+            }
+        }
+	    }
+	}
+};
+
+module.exports = roleHarvester;
