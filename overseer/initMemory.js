1cc5a18309de49924a7c8b1b4ff7e08c738762f6
Patrick <prehn@students.uni-mainz.de>
2016-12-08 17:55:21 +0100

    dasd

---
+++ overseer/initMemory.js
@@ -0,0 +1,52 @@
+var initMemory = {
+
+    /** @param {Creep} creep **/
+    run: function(roomName) {
+        var room = Game.rooms[roomName];
+        if(!room.memory.sources){//If this room has no sources memory yet
+            room.memory.sources = {}; //Add it
+            var sources = room.find(FIND_SOURCES);//Find all sources in the current room
+            for(var i in sources){
+                var source = sources[i];
+                source.memory = room.memory.sources[source.id] = {}; //Create a new empty memory object for this source
+                //Now you can do anything you want to do with this source
+                //for example you could add a worker counter:
+                source.memory.workers = 0;
+            }
+        }else{
+            var sources = room.find(FIND_SOURCES);//Find all sources in the current room
+            for(var i in sources){
+                var source = sources[i];
+                if(!source.memory){
+                    source.memory = room.memory.sources[source.id] = {}; //Create a new empty memory object for this source
+                    //Now you can do anything you want to do with this source
+                    //for example you could add a worker counter:
+                    source.memory.workers = 0;
+                 }
+            }
+        }
+        if(!room.memory.containers){
+            room.memory.containers={};
+            var containers = room.find(FIND_STRUCTURES,{
+                filter: (structure) => structure.structureType == STRUCTURE_CONTAINER});
+            for(var i in containers){
+                var container =containers[i];
+                container.memory= room.memory.containers[container.id]={};
+            }
+        }else{
+            var containers = room.find(FIND_STRUCTURES,{
+                filter: (structure) => structure.structureType == STRUCTURE_CONTAINER});
+            for(var i in containers){
+                var container =containers[i];
+                if(!container.memory){
+                    container.memory= room.memory.containers[container.id]={};
+                }
+            }
+
+
+
+        }
+	}
+};
+
+module.exports = initMemory;
--- overseer/main.js
+++ overseer/main.js
@@ -1,6 +1,8 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+//var roleHarvester = require('role.harvester');
+var roleMiner = require('role.miner');
+var phase1 = require('room.phase1');
+var blueprints = require('blueprints');
+var initMemory = require('initMemory');
 
 
 //Lets first add a shortcut prototype to the sources memory:
@@ -13,19 +15,7 @@ Source.prototype.memory = undefined;
 module.exports.loop = function () {
 
     for(var roomName in Game.rooms){//Loop through all rooms your creeps/structures are in
-        var room = Game.rooms[roomName];
-        if(!room.memory.sources){//If this room has no sources memory yet
-            room.memory.sources = {}; //Add it
-            var sources = room.find(FIND_SOURCES);//Find all sources in the current room
-            for(var i in sources){
-                var source = sources[i];
-                source.memory = room.memory.sources[source.id] = {}; //Create a new empty memory object for this source
-                //Now you can do anything you want to do with this source
-                //for example you could add a worker counter:
-                source.memory.workers = 0;
-                source.memory.occupied=0;
-            }
-        }
+        initMemory.run(roomName);
     }
 
 
