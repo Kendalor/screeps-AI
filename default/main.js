846e22f727a2d245d04c402427f3a53d82baa992
Patrick <prehn@students.uni-mainz.de>
2016-12-10 05:09:09 +0100

    dasdas

--- default/main.js
+++ default/main.js
@@ -48,7 +48,7 @@ module.exports.loop = function () {
             if(!Memory.squads[Game.flags[flag].name]){
                 Memory.squads[Game.flags[flag].name]= {}
                 Memory.squads[Game.flags[flag].name].members={};
-                Memory.squads[Game.flags[flag].name].size=2;
+                Memory.squads[Game.flags[flag].name].size=5;
                 Memory.squads[Game.flags[flag].name].target=flag;
                 Memory.squads[Game.flags[flag].name].assembled=false;
                 Memory.squads[Game.flags[flag].name].reached=false;
@@ -59,33 +59,7 @@ module.exports.loop = function () {
     }
 
 
-    for(var squad in Memory.squads){
-        if(!Game.flags[squad]){
-            delete Memory.squads[squad];
-            for(creep in Game.creeps){
-             var cr = Game.creeps[creep];
-             if(cr.memory.role == 'attacker'){
-                cr.suicide();
-             }
-            }
 
-        }
-
-        if(Object.keys(Memory.squads[squad].members).length < Memory.squads[squad].size && !Memory.squads[squad].assembled){
-            //console.log('Spawning');
-            console.log(Game.spawns['Spawn1'].createCreep(
-            [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'attacker', squad: squad, target: Memory.squads[squad].target}))
-            /*if(Game.spawns['Spawn1'].createCreep([ATTACK,ATTACK,TOUGH,TOUGH,TOUGH], undefined, {role: 'attacker', squad: squad, target: Memory.squads[squad].target})){
-                Memory.squads[squad].count = Memory.squads[squad].count+1;
-            }*/
-
-        }else if(Object.keys(Memory.squads[squad].members).length == Memory.squads[squad].size && !Memory.squads[squad].assembled){
-            Memory.squads[squad].assembled = true;
-            console.log('Squad assembled');
-        }
-
-
-    }
 
 
 
@@ -169,6 +143,34 @@ module.exports.loop = function () {
         */
 
     }
+    }
+
+        for(var squad in Memory.squads){
+        if(!Game.flags[squad]){
+            delete Memory.squads[squad];
+            for(creep in Game.creeps){
+             var cr = Game.creeps[creep];
+             if(cr.memory.role == 'attacker' && cr.memory.squad == 'squad'){
+                cr.suicide();
+             }
+            }
+
+        }
+
+        if(Object.keys(Memory.squads[squad].members).length < Memory.squads[squad].size && !Memory.squads[squad].assembled){
+            //console.log('Spawning');
+            console.log(Game.spawns['Spawn1'].createCreep(
+            [TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,ATTACK,ATTACK,ATTACK,ATTACK,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE,MOVE], undefined, {role: 'attacker', squad: squad, target: Memory.squads[squad].target}))
+            /*if(Game.spawns['Spawn1'].createCreep([ATTACK,ATTACK,TOUGH,TOUGH,TOUGH], undefined, {role: 'attacker', squad: squad, target: Memory.squads[squad].target})){
+                Memory.squads[squad].count = Memory.squads[squad].count+1;
+            }*/
+
+        }else if(Object.keys(Memory.squads[squad].members).length == Memory.squads[squad].size && !Memory.squads[squad].assembled){
+            Memory.squads[squad].assembled = true;
+            console.log('Squad assembled');
+        }
+
+
     }
 
 
--- default/role.attacker.js
+++ default/role.attacker.js
@@ -5,20 +5,44 @@ var roleAttacker = {
 
 
         if(!creep.spawning){
+        //console.log('TEST1');
         if(creep.memory.reached){
             var closestHostile = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            var closestStr =creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES,{filter: (str) => str.structureType != STRUCTURE_CONTROLLER && str.structureType != STRUCTURE_WALL});
+            var spawn = creep.pos.findClosestByRange(FIND_HOSTILE_STRUCTURES,{filter: (str) => str.structureType == STRUCTURE_TOWER});
+            if(spawn){
+                //console.log(creep.name);
+                //console.log('TEST2');
+                if(creep.attack(spawn) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(spawn);
+                    creep.say('attacking');
+                }
+            }else if(closestStr){
 
-            if(closestHostile) {
-                if(creep.attack(closestHostile) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(closestHostile);
+                if(creep.attack(closestStr) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(closestStr);
+                    creep.say('attacking');
                 }
-                }
-            creep.say('attacking');
+            }
+            else{
+                //console.log('TEST3');
+                creep.memory.reached=false;
+                delete creep.memory.target;
+
+            }
+
+        }else if(!creep.memory.reached && !creep.memory.target){
+
+
+            creep.memory.target=creep.memory.squad;
+            //console.log('TEST4');
         }else{
+            //console.log('TEST5');
+
             if(Memory.squads[creep.memory.squad].assembled){
                 var target=Game.flags[creep.memory.target];
-                console.log(target);
-                creep.moveTo(target,{costCallback: function(roomName, costMatrix) {
+                //console.log(target);
+                creep.moveTo(target/*,{costCallback: function(roomName, costMatrix) {
 	                if(roomName == 'E79N43') {
                         for(var i=0;i<255;i++){
                             for(var j=0;j<255;j++){
@@ -28,8 +52,8 @@ var roleAttacker = {
 		            }
 	            }
 
-                });
-                console.log(creep.moveTo(target,{ignoreDestructibleStructures: true}));
+                }*/);
+                creep.moveTo(target,{ignoreDestructibleStructures: true});
                 //if(creep.room.name = target.room.name){}
                 if(creep.pos.inRangeTo(target,2)){
                     creep.memory.reached=true;
