c383cb538edad7b43a86c42a1a707e503ef8643b
Pallony <gamingpalle@gmx.de>
2017-01-03 02:28:45 +0100

    Just for the looks

--- pallesCode/flag.handler.js
+++ pallesCode/flag.handler.js
@@ -16,46 +16,46 @@ module.exports = {
     run: function() {
         //Travel to Location
         for(var flag in Game.flags){
-            if(Game.flags[flag].color == COLOR_RED && Game.flags[flag].secondaryColor == COLOR_RED){
-
+			if (this.colorMatch(flag,COLOR_RED,COLOR_RED)){ // RED/RED
                 attackOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-            }else if(Game.flags[flag].color == COLOR_RED && Game.flags[flag].secondaryColor == COLOR_BLUE){
+				
+            }else if (this.colorMatch(flag,COLOR_RED,COLOR_BLUE)){ //RED/BLUE
                 defendOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-            }else if(Game.flags[flag].color == COLOR_WHITE && Game.flags[flag].secondaryColor == COLOR_WHITE){
+				
+            }else if (this.colorMatch(flag,COLOR_WHITE,COLOR_WHITE)){ //WHITE/WHITE
                 scoutingOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-            }else if(Game.flags[flag].color == COLOR_WHITE && Game.flags[flag].secondaryColor == COLOR_GREY){
+				
+            }else if (this.colorMatch(flag,COLOR_WHITE,COLOR_GREY)){ //WHITE/GREY
                 reserveOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-            }else if(Game.flags[flag].color == COLOR_GREEN){
+				
+            }else if (this.colorMatch(flag,COLOR_GREEN,COLOR_GREEN)){ //GREEN/GREEN
                 tankOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-
-            }else if(Game.flags[flag].color == COLOR_BLUE && Game.flags[flag].secondaryColor == COLOR_RED){
+				
+            }else if (this.colorMatch(flag,COLOR_BLUE,COLOR_RED)){ //BLUE/RED
                 thiefOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
-
-            }
-            
-            else if(Game.flags[flag].color == COLOR_WHITE && Game.flags[flag].secondaryColor == COLOR_GREEN){
+				
+            }else if (this.colorMatch(flag,COLOR_WHITE,COLOR_GREEN)){ // WHITE/GREEN
                 colonizeOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
 
-            }
-			else if(Game.flags[flag].color == COLOR_WHITE && Game.flags[flag].secondaryColor == COLOR_YELLOW){
+			}else if (this.colorMatch(flag,COLOR_WHITE,COLOR_YELLOW)){ //WHITE/YELLOW
                 remoteBuildOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
 
-            }
-            else if(Game.flags[flag].color == COLOR_WHITE && Game.flags[flag].secondaryColor == COLOR_BLUE){
+            }else if (this.colorMatch(flag,COLOR_WHITE,COLOR_BLUE)){ //WHITE/BLUE
                 devAidOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
 
-            }
-            else if(Game.flags[flag].color == COLOR_BLUE && Game.flags[flag].secondaryColor == COLOR_BLUE){
+            }else if (this.colorMatch(flag,COLOR_BLUE,COLOR_BLUE)){ //BLUE/BLUE
                 remoteMiningOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
 
-            }
-			
-			else if(Game.flags[flag].color == COLOR_RED && Game.flags[flag].secondaryColor == COLOR_BLUE){
+            }else if (this.colorMatch(flag,COLOR_RED,COLOR_GREEN)){ //RED/GREEN
                 penetrationOperation.init(Game.flags[flag].pos.roomName,Game.flags[flag].name);
 
             }
         }
-    }
+    },
+	
+	colorMatch: function(flag,color1,color2) {
+		return (Game.flags[flag].color == color1 && Game.flags[flag].secondaryColor == color2)
+	}
 
 
 };
