6f1c7ff7db3832cb54268565cb7c9eff821c75d2
Pallony <gamingpalle@gmx.de>
2017-01-05 19:13:25 +0100

    Added memory to structures

--- sim/prototypes.js
+++ sim/prototypes.js
@@ -179,6 +179,24 @@ module.exports = function(){
 	/*
 	* STUCTURE
 	*/
+	Object.defineProperties(Structure.prototype,{ 
+		'memory' : {
+			get: function() {
+				if (Memory.structures === undefined) {
+					Memory.structures = {};
+				}
+				if (Memory.structures[this.id] === undefined) {
+					Memory.structures[this.id] = {};
+				}
+				return Memory.structures[this.id];
+			},
+			set: function(v) {
+				return _.set(Memory, 'structures.' + this.id, v);
+			},
+			configurable: true,
+			enumerable: false
+		}
+	);
 
 	/*
 	* OWNED_STRUCTURE
