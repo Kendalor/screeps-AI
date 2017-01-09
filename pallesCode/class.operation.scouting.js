de366da01b16e9e9f30cc0df4a48d11237e0110b
Patrick <prehn@students.uni-mainz.de>
2017-01-09 02:53:38 +0100

    jh

--- pallesCode/class.operation.scouting.js
+++ pallesCode/class.operation.scouting.js
@@ -5,13 +5,13 @@ module.exports = class{
         }
         static run(id){
             // tank
-            var creep_body = [MOVE];
+            var creep_body = [MOVE,MOVE,MOVE,MOVE,MOVE,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH,TOUGH];
             // scout
             //var creep_body = [MOVE];
             if(!this.checkForDelete(id)){ // RUN ONLY IF APPLICABLE
                 if(!Memory.operations[id].creep){ //DOES THIS OPERATION ALREADY HAVE A CREEP?
-                    if(Game.spawns['Spawn1'].canCreateCreep(creep_body,undefined,{role: 'scout', operation_id: id}) == OK){// NO SPAWN IT IF POSSIBLE !
-                        var name=Game.spawns['Spawn1'].createCreep(creep_body,undefined,{role: 'scout', operation_id: id});
+                    if(Game.spawns['Spawn3'].canCreateCreep(creep_body,undefined,{role: 'scout', operation_id: id}) == OK){// NO SPAWN IT IF POSSIBLE !
+                        var name=Game.spawns['Spawn3'].createCreep(creep_body,undefined,{role: 'scout', operation_id: id});
                         var creep=Game.creeps[name];
                         Memory.operations[id].creep=name;
                     }
